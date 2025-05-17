3c9321d86f84686f640cd4bc4eeb0ca5e6700c69
Kajani Y <kajani@Kajanis-MacBook-Air.local>
2025-05-17 15:35:38 -0400

    Initial Project Setup

---
+++ build/CMakeFiles/MarsRoverAutonomySim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarsRoverAutonomySim.dir/src/main.cpp.o"
+  "CMakeFiles/MarsRoverAutonomySim.dir/src/main.cpp.o.d"
+  "MarsRoverAutonomySim"
+  "MarsRoverAutonomySim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MarsRoverAutonomySim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Mars Rover Autonomy Simulation Started!" << std::endl;
+    return 0;
+}
